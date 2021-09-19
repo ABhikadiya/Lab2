8dfde32b7d7f5c6ecdc47484bd12cb397829d4af
Anish <anishbhikadiya@gmail.com>
2021-09-18 23:41:34 -0400

    first Hypotenuse test fail

---
+++ NunitTestTriangle.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace lab2_abhikadiya
+{
+
+    [TestClass]
+    class NunitTestTriangle
+    {
+        [TestMethod]
+        public void TestGetHypotenuse1()
+        {
+            Triangle obj = new Triangle();
+            double result = obj.GetHypotenuse(30, 50);
+            Assert.AreEqual(58.31, result);
+        }
+    }
+}
