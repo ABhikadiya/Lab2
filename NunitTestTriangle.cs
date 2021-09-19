d35f4bcbab6b3371d7addaabd922935e20541be0
Anish <anishbhikadiya@gmail.com>
2021-09-18 23:49:12 -0400

    First area  test fail

--- NunitTestTriangle.cs
+++ NunitTestTriangle.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace lab2_abhikadiya
 {
@@ -23,6 +24,14 @@ namespace lab2_abhikadiya
             Triangle obj = new Triangle();
             double result = obj.GetHypotenuse(10.80, 5.20);
             Assert.AreEqual(11.99, result);
+        }
+
+        [TestMethod]
+        public void TestGetArea1()
+        {
+            Triangle obj = new Triangle();
+            double result = obj.GetArea(10, 20);
+            Assert.AreEqual(100, result);
         }
     }
 }
