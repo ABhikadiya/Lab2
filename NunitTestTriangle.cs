8333d859c18feb95cbed2c682cd0c8a1e99d9f58
Anish <anishbhikadiya@gmail.com>
2021-09-18 23:43:50 -0400

    second Hypotenuse test fail

--- NunitTestTriangle.cs
+++ NunitTestTriangle.cs
@@ -14,5 +14,15 @@ namespace lab2_abhikadiya
             double result = obj.GetHypotenuse(30, 50);
             Assert.AreEqual(58.31, result);
         }
+
+
+
+        [TestMethod]
+        public void TestGetHypotenuse2()
+        {
+            Triangle obj = new Triangle();
+            double result = obj.GetHypotenuse(10.80, 5.20);
+            Assert.AreEqual(11.99, result);
+        }
     }
 }
