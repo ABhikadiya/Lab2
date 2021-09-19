ccb2e25e22e9d18b535a4fb2c098735bf1c1494e
Anish <anishbhikadiya@gmail.com>
2021-09-18 23:46:10 -0400

    All Hypotenuse test pass

---
+++ Triangle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_abhikadiya
+{
+    class Triangle
+    {
+        public double GetHypotenuse(double side1, double side2)
+        {
+
+            return Math.Round(Math.Sqrt(Math.Pow(side1, 2) + Math.Pow(side2, 2)), 2);
+        }
+    }
+}
